30f6365decf0cc51f8a82c56f46f0e11c5310e10
HabeebaNazrin <habeebanazrin1406@gmail.com>
2024-02-07 10:44:52 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_test/firebase_options.dart';
 import 'package:firebase_test/homepage.dart';
 import 'package:firebase_test/loginpage.dart';
 import 'package:flutter/material.dart';
 
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   User? user = FirebaseAuth.instance.currentUser;
+//   runApp(MaterialApp(
+//     debugShowCheckedModeBanner: false,
+//     home: user == null ? const LoginPage() : const HomePage(),
+//   ));
+// }
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  User? user = FirebaseAuth.instance.currentUser;
-  runApp(MaterialApp(
-    home: user == null ? LoginPage() : HomePage(),
-  ));
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: LoginPage()));
 }
